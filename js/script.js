cd7d9276cd3f136f8dceb9a70518234218e16a98
0sorojo <redrootproductions@gmail.com>
2020-06-05 17:29:19 -0400

    final review ready for turn in

--- js/script.js
+++ js/script.js
@@ -1,28 +1,29 @@
 let vacationType = "";
 let lowerCaseVacationType = "";
-let groupSize;
-let result;
 let destination;
+let groupSize;
 let via;
+let result;
 
-vacationType = prompt("Hi! So glad you could stop by. I'm here to help with your vacation plans. What kind of vacation theme are you interested in: Musical, Tropical, or Adventurous? \n Please type your answer below...");
+
+vacationType = prompt("Hi! So glad you could stop by. I'm here to help with your vacation plans. What kind of vacation theme are you interested in:\n \nMusical, Tropical, or Adventurous? \n \nPlease type your answer below...");
 lowerCaseVacationType = vacationType.toLowerCase();
 
 while (!~["musical", "tropical", "adventurous"].indexOf(lowerCaseVacationType)) {
-    vacationType = prompt("I apologize... I should have been more specific: please choose from: Musical, Tropical, or Adventurous and type the word into the box checking for spelling");
+    vacationType = prompt("I apologize... I should have been more specific: please choose from:\n\nMusical \n\nTropical \n\nAdventurous \n\nthen type the word into the box, checking for spelling");
     lowerCaseVacationType = vacationType.toLowerCase();
 }
 
-groupSize = prompt("Interesting! Wow, that's a cool choice. \n How many people do you think you'll travel with?");
+groupSize = prompt("Interesting! Wow, that's a cool choice! \nHow many people will be traveling...including yoursel?");
 
-while (groupSize != parseInt(groupSize)) { 
-    groupSize = prompt("Thanks for that but I need the number of people. like 1 or 2 or 15");
+while ((groupSize != parseInt(groupSize)) || (groupSize <= 0)) { 
+    groupSize = prompt("Thanks for that, but I need the NUMBER of people. like 1 or 2 or 15");
 }
 
 if (lowerCaseVacationType === "musical") {
     destination = "to New Orleans";
 } else if (lowerCaseVacationType === "tropical") {
-    destination = "to a Beach Vacation in Mexico";
+    destination = "on a Beach Vacation in Mexico";
 } else if (lowerCaseVacationType === "adventurous") {
     destination = "White Water Rafting in the Grand Canyon!!";
 }
@@ -35,7 +36,7 @@ if (groupSize <= 2) {
     via = "Charter Flight";
 }
 
-result = "I think that if you are looking for a " + vacationType + " theme, I would suggest you go " + destination + ". \n Now for transportation...hmmm...well since you have " + groupSize + " people going, I'd suggest going by " + via;
+result = "I think that if you are looking for a " + vacationType + " theme, I would suggest you go " + destination + ". \n\nNow lets think about transportation...\n\nhmmm...weelll, since you have " + groupSize + " people going, I'd suggest going by " + via + ".";
 
 console.log(result);
 
